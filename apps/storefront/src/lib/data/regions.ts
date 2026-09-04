78ebf9af405994cb8dba37c4c1a3fd0eadf862af
luijten1990 <luijtem@gmail.com>
2026-09-04 23:26:09 +0700

    Fetch current region configuration without persistent cache

--- apps/storefront/src/lib/data/regions.ts
+++ apps/storefront/src/lib/data/regions.ts
@@ -2,44 +2,27 @@
 
 import { sdk } from "@lib/config"
 import { HttpTypes } from "@medusajs/types"
-import { getCacheOptions } from "./cookies"
 
 export const listRegions = async () => {
-  const next = {
-    ...(await getCacheOptions("regions")),
-  }
-
   return await sdk.client
     .fetch<{ regions: HttpTypes.StoreRegion[] }>(`/store/regions`, {
       method: "GET",
-      next,
-      cache: "force-cache",
+      cache: "no-store",
     })
     .then(({ regions }) => regions)
 }
 
 export const retrieveRegion = async (id: string) => {
-  const next = {
-    ...(await getCacheOptions(["regions", id].join("-"))),
-  }
-
   return await sdk.client
     .fetch<{ region: HttpTypes.StoreRegion }>(`/store/regions/${id}`, {
       method: "GET",
-      next,
-      cache: "force-cache",
+      cache: "no-store",
     })
     .then(({ region }) => region)
 }
 
-const regionMap = new Map<string, HttpTypes.StoreRegion>()
-
 export const getRegion = async (countryCode: string) => {
   countryCode = countryCode.toLowerCase()
-
-  if (regionMap.has(countryCode)) {
-    return regionMap.get(countryCode)
-  }
 
   const regions = await listRegions()
 
@@ -47,6 +30,7 @@ export const getRegion = async (countryCode: string) => {
     return null
   }
 
+  const regionMap = new Map<string, HttpTypes.StoreRegion>()
   regions.forEach((region) => {
     region.countries?.forEach((c) => {
       regionMap.set(c?.iso_2 ?? "", region)
